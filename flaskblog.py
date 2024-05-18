b3e34e7b3859ce8c66a93ef9c89c0f83a725c277
mrunalpatil1198 <mrunalpatil1198@gmail.com>
2024-05-17 21:09:50 -0400

    Created project and added html templates and styles

---
+++ flaskblog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Mrunal Patil',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Nov 1 2020'
+    },
+    {
+        'author': 'Jon Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Apr 21 2021'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
